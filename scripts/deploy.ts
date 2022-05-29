bfdbbb0d7059867ac78ae1d499240ace39bad24b
Filip Alexandrov <filipalexandrov@gmail.com>
2022-05-29 11:53:13 +0200

    web app first design
    Web app design considerations. Customized to the project.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,7 @@ import hre from "hardhat";
 const ethers = hre.ethers;
 
 async function main() {
-  const VotingToken = await ethers.getContractFactory("VotingToken");
+ /*  const VotingToken = await ethers.getContractFactory("VotingToken");
   const votingContract = await VotingToken.deploy();
   await votingContract.deployed();
 
@@ -19,8 +19,8 @@ async function main() {
 
   console.log("Timelock Controller deployed to: ", timelockContract.address);
 
-  const voteTokenAddress = "0x70eACA707914549951cE39e816BF27954fB067d4";
-  const timelockAddress = "0x7Fa7eC42546072Ad1ae3FF993801fe9890cC33C9";
+  const voteTokenAddress = votingContract.address;
+  const timelockAddress = timelockContract.address;
   const VOTING_DELAY = 2; // blocks delay before voting starts
   const VOTING_PERIOD = 5; // blocks voting duration
   const MIN_VOTES_TO_PROPOSE = 10; // tokens
@@ -41,23 +41,33 @@ async function main() {
 
   // Wait 10 blocks to allow solidity code to get
   // on etherscan backend
-  await governorContract.deployTransaction.wait(10);
+  await governorContract.deployTransaction.wait(10); 
+
 
   // Verify contract source code
   await hre.run("verify:verify", {
-    address: timelockContract.address,
+    address: "0x76Fe17508A2ecaA198a9B52f5Aad6d4A7a3a549A",
     constructorArguments: [MIN_DELAY, [], []],
     contract: "contracts/dao/Timelock.sol:Timelock",
   });
+  
 
   await hre.run("verify:verify", {
-    address: votingContract.address,
+    address:"0x7A2bcC2fE5c7c97B9c95cD77c2eC96b6a2420E2F",
     constructorArguments: [],
     contract: "contracts/dao/ERC20Votes.sol:VotingToken",
   });
+ */ 
+
+  const voteTokenAddress = "0x7A2bcC2fE5c7c97B9c95cD77c2eC96b6a2420E2F";
+  const timelockAddress = "0x76Fe17508A2ecaA198a9B52f5Aad6d4A7a3a549A";
+  const VOTING_DELAY = 2; // blocks delay before voting starts
+  const VOTING_PERIOD = 5; // blocks voting duration
+  const MIN_VOTES_TO_PROPOSE = 10; // tokens
+  const QUORUM_PERCENTAGE = 51; // min 50% have to vote
 
   await hre.run("verify:verify", {
-    address: governorContract.address,
+    address: "0x278Dfd116Ac51449459a96b3412F058582CafE8F",
     constructorArguments: [
       voteTokenAddress,
       timelockAddress,
